0d5ea9066deef51d96ce82e55d37d4ee6d34fc49
yangsungsoo <ysotgood@gmail.com>
2022-09-14 23:07:58 +0900

    sucess_JS_Browser_#4_Login

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const h1=document.querySelector('div.hello:first-child h1')
+
+const loginForm=document.querySelector('#login-form')
+const loginInput=document.querySelector('#login-form input')
+const greeting=document.querySelector('#greeting')
 
 
-function handleTitleClick(){
-    h1.classList.toggle('active')
 
-} 
+const HIDDEN_CLASSNAME='hidden'
+const USERNAME_KEY='username'
 
-h1.addEventListener("click",handleTitleClick)
+
+function onLoginSubmit(event){
+    event.preventDefault()
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    const username=loginInput.value
+    localStorage.setItem(USERNAME_KEY,username)
+    paintGreetings(savedUsername)
+}
+
+
+
+
+function paintGreetings(username){
+    greeting.innerText=`Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+
+}
+
+const savedUsername=localStorage.getItem(USERNAME_KEY)
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener('submit',onLoginSubmit)
+}else{
+  paintGreetings(savedUsername)
+}
